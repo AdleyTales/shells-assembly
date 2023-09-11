46b507880402b91f39f0ec702667d79a39da4164
adleytales <adleytales@126.com>
2023-09-11 17:25:23 +0800

    m sync bash

--- sync-git.bash
+++ sync-git.bash
@@ -10,21 +10,18 @@ GITLAB_PROJECT_DIR=../gitlab/$PROJECT_NAME
 GITHUB_PROJECT_DIR=../../github/$PROJECT_NAME
 GITEE_PROJECT_DIR=../../gitee/$PROJECT_NAME
 
-
-
-
 if [[ -z "$PROJECT_NAME" ]];then
   echo "请输入工程名"
   exit 1
 fi
 
+if [[ -z "$GIT_COMMIT_MESSAGE" ]];then
+  echo "请输入git commit message"
+  exit 1
+fi
+
 # 直接拷贝源代码到不同的git库下
 cpFunc() {
-  if [[ -z "$GIT_COMMIT_MESSAGE" ]];then
-    echo "请输入git commit message"
-    exit 1
-  fi
-
   cp -rf $PROJECT_NAME ../gitlab/
   cp -rf $PROJECT_NAME ../github/
   cp -rf $PROJECT_NAME ../gitee/
@@ -40,8 +37,6 @@ gitShellFunc() {
 gitlabFunc() {
   echo "---------------------------------------------------- git sync gitlab begin ..."
   cd $GITLAB_PROJECT_DIR
- 
-
 	gitShellFunc
   echo "---------------------------------------------------- git sync gitlab success!"
 }
@@ -60,66 +55,13 @@ giteeFunc() {
   echo "---------------------------------------------------- git sync gitee success!"
 }
 
-# init gitlab
-gitlabInitFunc() {
-  echo "---------------------------------------------------- git init gitlab begin ..."
-  cd ../gitlab
-  mkdir $PROJECT_NAME
-  cd shells-assembly
-  git init 
-  git add .
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin git@gitlab.com:AdleyTales/shells-assembly.git
-  git push -u origin main
-  echo "---------------------------------------------------- git init gitlab push origin success!"
-}
-
-# init github
-githubInitFunc() {
-  echo "---------------------------------------------------- git init gitee begin ..."
-  cd ../../github
-  mkdir $PROJECT_NAME
-  cd shells-assembly
-  git init 
-  git add .
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin git@github.com:AdleyTales/shells-assembly.git
-  git push -u origin main
-  echo "---------------------------------------------------- git init gitee push origin success!"
-}
-
-# init gitee
-giteeInitFunc() {
-  echo "---------------------------------------------------- git init gitee begin ..."
-  cd ../../gitee
-  mkdir $PROJECT_NAME
-  cd shells-assembly
-  git init 
-  git add .
-  git commit -m "first commit"
-  git remote add origin git@gitee.com:adleytales/shells-assembly.git
-  git push -u origin "master" 
-  echo "---------------------------------------------------- git init gitee push origin success!"
-}
-
 main() {
-  # 判断是否需要初始化
-  if [ ! -d "../gitlab/$PROJECT_NAME" ];then 
-    gitlabInitFunc
-  elif [ ! -d "../../github/$PROJECT_NAME" ];then 
-    githubInitFunc
-  elif [ ! -d "../../gitee/$PROJECT_NAME" ];then 
-    giteeInitFunc
-  else
-    echo "开始同步 ..."
-    cpFunc 
-    gitlabFunc
-    githubFunc
-    giteeFunc
-    echo "同步完成！"
-  fi
+  echo "开始同步 ..."
+  cpFunc 
+	gitlabFunc
+	githubFunc
+	giteeFunc
+  echo "同步完成！"
 }
 
 main
